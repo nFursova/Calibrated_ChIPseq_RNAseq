df0cfaa659fc41ef3d4574642008f1e089a6dc92
Nadezda Fursova <nfursova.msu@gmail.com>
2021-05-12 11:02:18 -0400

    Update ChIPseq_Plot_ChromosomeWide_Read_Coverage.R

--- ChIPseq/ChIPseq_Plot_ChromosomeWide_Read_Coverage.R
+++ ChIPseq/ChIPseq_Plot_ChromosomeWide_Read_Coverage.R
@@ -59,10 +59,12 @@ chromosome_density=function(x,y,z){
   plot=melt(plot)
   plot$coordinate=rep(seq(from=1, to = length(UNT[which(seqnames(UNT)==y)])*250000, by = 250000),2)
   
-  #####Order for plotting: first plot TAM as it is higher and should be at the back
-  #####The order for plotting can be switched using factor levels
+  #####Order for plotting: first plot TAM as it has higher values in this case and looks better when it is at the back
+  #####This can be changed by changing the order of factor levels
   
   plot$variable=factor(plot$variable, levels=c('TAM', 'UNT'))
+  
+  #####Calculate the maximum value for the y-scale
   
   ymax = round(max(plot$value), digits = -1)*1.5
   
@@ -74,7 +76,7 @@ chromosome_density=function(x,y,z){
     theme_bw()+
     theme(axis.line = element_line(colour = 'black', size = 1.5), 
           axis.ticks = element_line(colour = "black", size = 1.5), 
-          plot.title = element_text(size=20), 
+          plot.title = element_text(size=15, hjust = 0.5), 
           axis.text.x = element_text(size=20, colour = "black"), 
           axis.text.y = element_text(size=20, colour="black"), 
           axis.title.x=element_text(size=20, colour="black"), 
@@ -87,8 +89,9 @@ chromosome_density=function(x,y,z){
                        breaks=seq(0, length(UNT[which(seqnames(UNT)==y)])*250000, by = 25000000), 
                        labels=paste0(seq(0, length(UNT[which(seqnames(UNT)==y)])*250000, by = 25000000)/1000000, ' Mb'))+
     scale_y_continuous(expand = c(0,0), name='Read Density', limits=c(0, ymax), breaks = seq(0,ymax, by = 5))+
-    scale_fill_manual(values=c('#ed2224', '#29286b'), labels=c('TAM', 'UNT'))+scale_alpha_manual(values = c(1, 1), guide='none')+
-    ggtitle(paste0(z, ' in ', x))+theme(plot.title = element_text(size=15, hjust = 0.5))+
+    scale_fill_manual(values=c('#ed2224', '#29286b'), labels=c('TAM', 'UNT'))+
+    scale_alpha_manual(values = c(1, 1), guide='none')+
+    ggtitle(paste0(z, ' in ', x))+
     geom_area(position='identity', alpha = 1)
   plot(p)
   
