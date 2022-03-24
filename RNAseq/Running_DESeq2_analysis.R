a39f672b2c96885084719ba136c2508a4506a7db
Nadezda Fursova <nfursova.msu@gmail.com>
2022-03-24 12:59:28 -0400

    Update Running_DESeq2_analysis.R

--- RNAseq/Running_DESeq2_analysis.R
+++ RNAseq/Running_DESeq2_analysis.R
@@ -10,6 +10,7 @@ dir.create('plots/PCAplots')
 dir.create('plots/VolcanoPlots')
 
 dir.create('results') #Output data tables will be saved here
+dir.create('results/SizeFactors')
 dir.create('results/DESeq2OutputTables')
 dir.create('results/DiffGeneBodyCoordinates')
 dir.create('results/DiffRefSeqID')
@@ -18,9 +19,8 @@ dir.create('results/DiffRefSeqID')
 ########Run DESeq2 script for spike-in calibrated RNA-seq data
 ########This version performs differential gene expression analysis for two conditions (UNT vs TAM) and 3 biological replicates
 
-
-cell_line = c('BAP1ff', 'PRC1CPM_BAP1ff', 'PRC1CPM') #Cell line names should match the ones in the read count file
-rnaseq_app = 'Nuc' #For Nuclear RNA-seq name convention
-cutoff=1.5 #Fold-change cut-off for calling DEGs
+cell_line <-  c('BAP1ff', 'PRC1CPM_BAP1ff', 'PRC1CPM') #Cell line names should match the ones in the read count file
+rnaseq_app <-  'Nuc' #For Nuclear RNA-seq name convention
+cutoff <- 1.5 #Fold-change cut-off for calling DEGs
 
 source('RNAseq_DESeq2.R')
